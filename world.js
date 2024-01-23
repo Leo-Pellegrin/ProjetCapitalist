b0f67b8bf49e58836bf5631d9843d5d147fe3816
Leo-Pellegrin <leo.pellegrin87@gmail.com>
2024-01-23 16:58:33 +0100

    Mise en place de graphql

---
+++ world.js
@@ -0,0 +1,247 @@
+module.exports = {
+    "name": "Nom du monde",
+    "logo": "icones/logomonde.jpg",
+    "money": 0,
+    "score": 0,
+    "totalangels": 0,
+    "activeangels": 0,
+    "angelbonus": 2,
+    "lastupdate": 0,
+    "products": [
+        {
+            "id": 1,
+            "name": "Filtre",
+            "logo": "icones/filtre.png",
+            "cout": 4,
+            "croissance": 1.07,
+            "revenu": 1,
+            "vitesse": 500,
+            "quantite": 1,
+            "timeleft": 0,
+            "managerUnlocked": false,
+            "palliers": [
+                {
+                    "name": "Palier1",
+                    "logo": "icones/premierpalier.jpg",
+                    "seuil": 20,
+                    "idcible": 1,
+                    "ratio": 2,
+                    "typeratio": "vitesse",
+                    "unlocked": false
+                },
+                {
+                    "name": "Palier2",
+                    "logo": "icones/deuxiemepalier.jpg",
+                    "seuil": 50,
+                    "idcible": 1,
+                    "ratio": 2,
+                    "typeratio": "vitesse",
+                    "unlocked": false
+                },
+                {
+                    "name": "Palier3",
+                    "logo": "icones/troisiemepalier.jpg",
+                    "seuil": 100,
+                    "idcible": 1,
+                    "ratio": 2,
+                    "typeratio": "vitesse",
+                    "unlocked": false
+                },
+            ]    
+    },
+        {
+            "id": 2,
+            "name": "Feuilles",
+            "logo": "icones/feuille.jpeg",
+            "cout": 60,
+            "croissance": 1.15,
+            "revenu": 60,
+            "vitesse": 3000,
+            "quantite": 1,
+            "timeleft": 0,
+            "managerUnlocked": false,
+            "palliers": [
+                {
+                    "name": "Palier1",
+                    "logo": "icones/premierpalier.jpg",
+                    "seuil": 20,
+                    "idcible": 1,
+                    "ratio": 2,
+                    "typeratio": "vitesse",
+                    "unlocked": false
+                },
+                {
+                    "name": "Palier2",
+                    "logo": "icones/deuxiemepalier.jpg",
+                    "seuil": 50,
+                    "idcible": 1,
+                    "ratio": 2,
+                    "typeratio": "vitesse",
+                    "unlocked": false
+                },
+                {
+                    "name": "Palier3",
+                    "logo": "icones/troisiemepalier.jpg",
+                    "seuil": 100,
+                    "idcible": 1,
+                    "ratio": 2,
+                    "typeratio": "vitesse",
+                    "unlocked": false
+                },
+            ]    
+        }
+    ],
+    "allunlocks": [
+        {
+            "name": "Nom du premier unlock général",
+            "logo": "icones/premierunlock.jpg",
+            "seuil": 30,
+            "idcible": 0,
+            "ratio": 2,
+            "typeratio": "gain",
+            "unlocked": false
+        },
+    ],
+    "upgrades": [
+        {
+            "name": "Petite clope",
+            "logo": "icones/petiteclope.jpg",
+            "seuil": 1e3,
+            "idcible": 1,
+            "ratio": 3,
+            "typeratio": "gain",
+            "unlocked": false
+        },
+        {
+            "name": "Clope industrielle",
+            "logo": "icones/petiteclope.jpg",
+            "seuil": 1e4,
+            "idcible": 2,
+            "ratio": 3,
+            "typeratio": "gain",
+            "unlocked": false
+        },
+        {
+            "name": "Clope industrielle",
+            "logo": "icones/petiteclope.jpg",
+            "seuil": 1e4,
+            "idcible": 2,
+            "ratio": 3,
+            "typeratio": "gain",
+            "unlocked": false
+        },
+        {
+            "name": "Clope industrielle",
+            "logo": "icones/petiteclope.jpg",
+            "seuil": 1e4,
+            "idcible": 2,
+            "ratio": 3,
+            "typeratio": "gain",
+            "unlocked": false
+        },
+        {
+            "name": "Clope industrielle",
+            "logo": "icones/petiteclope.jpg",
+            "seuil": 1e4,
+            "idcible": 2,
+            "ratio": 3,
+            "typeratio": "gain",
+            "unlocked": false
+        },
+        {
+            "name": "Clope industrielle",
+            "logo": "icones/petiteclope.jpg",
+            "seuil": 1e4,
+            "idcible": 2,
+            "ratio": 3,
+            "typeratio": "gain",
+            "unlocked": false
+        },
+        {
+            "name": "Clope industrielle",
+            "logo": "icones/petiteclope.jpg",
+            "seuil": 1e4,
+            "idcible": 2,
+            "ratio": 3,
+            "typeratio": "gain",
+            "unlocked": false
+        },
+        {
+            "name": "Clope industrielle",
+            "logo": "icones/petiteclope.jpg",
+            "seuil": 1e4,
+            "idcible": 2,
+            "ratio": 3,
+            "typeratio": "gain",
+            "unlocked": false
+        },
+        {
+            "name": "Clope industrielle",
+            "logo": "icones/petiteclope.jpg",
+            "seuil": 1e4,
+            "idcible": 2,
+            "ratio": 3,
+            "typeratio": "gain",
+            "unlocked": false
+        },
+        {
+            "name": "Clope industrielle",
+            "logo": "icones/petiteclope.jpg",
+            "seuil": 1e4,
+            "idcible": 2,
+            "ratio": 3,
+            "typeratio": "gain",
+            "unlocked": false
+        },
+
+    ],
+    "angelupgrades": [
+        {
+            "name": "Angel Sacrifice",
+            "logo": "icones/angel.jpeg",
+            "seuil": 10,
+            "idcible": 1,
+            "ratio": 3,
+            "typeratio": "gain",
+            "unlocked": false
+        },
+        {
+            "name": "Angel Sacrifice",
+            "logo": "icones/angel.jpeg",
+            "seuil": 30,
+            "idcible": 2,
+            "ratio": 3,
+            "typeratio": "gain",
+            "unlocked": false
+        },
+        {
+            "name": "Angel Sacrifice",
+            "logo": "icones/angel.jpeg",
+            "seuil": 50,
+            "idcible": 3,
+            "ratio": 3,
+            "typeratio": "gain",
+            "unlocked": false
+        },
+    ],
+    "managers": [
+        {
+            "name": "Philip Morris",            
+            "logo": "icones/icones/Philip_Morris_tobacconist.jpg",
+            "seuil": 10,
+            "idcible": 1,
+            "ratio": 0,
+            "typeratio": "gain",
+            "unlocked": false
+        },
+        {
+            "name": "Mélia Ferreira",            
+            "logo": "icones/icones/melia.jpg",
+            "seuil": 10,
+            "idcible": 2,
+            "ratio": 0,
+            "typeratio": "gain",
+            "unlocked": false
+        }
+    ]
+};
