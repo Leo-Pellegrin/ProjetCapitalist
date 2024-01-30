1b014f97fa6331f6427d17ce3486433ab4de9190
Leo-Pellegrin <leo.pellegrin87@gmail.com>
2024-01-30 09:16:14 +0100

    Correction bug majeur

--- bar-tabac/src/app/app.component.ts
+++ bar-tabac/src/app/app.component.ts
@@ -3,33 +3,20 @@ import { RouterOutlet } from '@angular/router';
 import {World, Palier, Product} from './world'; 
 import { WebserviceService } from './webservice.service';
 import { ProductComponent } from './product/product.component';
+import {GET_SERV } from '../request';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-<<<<<<< HEAD
-  imports: [RouterOutlet],
-  templateUrl: '../app.component.html',
-  title = 'bar-tabac',
-  world: World = new World(),
-  
-
-  constructor(private service: WebserviceService) {
-    this.service.getWorld().then(
-      world => {
-        this.world = world.data.getWorld;
-      }
-    );
-  }
-=======
   imports: [RouterOutlet, ProductComponent],
   templateUrl: './app.component.html',
->>>>>>> c503305a39d7395c87f14d6f4bd89b257dbc13d8
 })
 
 export class AppComponent{
-    title = 'bar-tabac'
-    world: World = new World()
+    title = 'bar-tabac';
+    world: World = new World();
+    server = GET_SERV;
 
     constructor(private service: WebserviceService) {
       this.service.getWorld().then(
