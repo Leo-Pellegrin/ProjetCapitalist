6e800ec45b99df529e59a3b39415ab7bb0b058ff
salomeroland <salome.roland@etud.univ-jfc.fr>
2024-01-30 08:28:00 +0100

    Avancement frontend

---
+++ bar-tabac/src/app/product/product.component.ts
@@ -0,0 +1,20 @@
+import { Component, Input } from '@angular/core';
+import { Product } from '../world';
+
+@Component({
+  selector: 'app-product',
+  standalone: true,
+  imports: [],
+  templateUrl: './product.component.html',
+  styleUrl: './product.component.css'
+})
+export class ProductComponent {
+
+  product : Product = new Product();
+
+  @Input()
+  set prod(value :Product){
+    this.product = value; 
+  }
+  
+}
