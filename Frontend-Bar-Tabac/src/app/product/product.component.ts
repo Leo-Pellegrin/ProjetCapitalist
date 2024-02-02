1518057d1da28a1b9ff6eefffa00d154f4052069
Leo-Pellegrin <leo.pellegrin87@gmail.com>
2024-02-02 02:22:21 +0100

    Correction double balise html + progression bar

--- Frontend-Bar-Tabac/src/app/product/product.component.ts
+++ Frontend-Bar-Tabac/src/app/product/product.component.ts
@@ -1,22 +1,57 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 import { Product } from '../world';
+import {MatProgressBarModule} from '@angular/material/progress-bar'
+import {GET_SERV} from '../../request';
 
 @Component({
   selector: 'app-product',
   standalone: true,
-  imports: [],
+  imports: [MatProgressBarModule],
   templateUrl: './product.component.html',
   styleUrl: './product.component.css'
 })
 
 export class ProductComponent {
+  product: Product = new Product();
+  lastupdate: number = Date.now();
 
-  product : Product = new Product();
+  @Input() 
+  set prod(value: Product) {
+      if(value != undefined){
+        this.product = value; 
+      }
+  }
   money : number = 0
+  progressbarvalue : number = 0
+  server = GET_SERV;
 
-  @Input()
-  set prod(value :Product){
-    console.log("value : " + value)
-    this.product = value; 
+  onClick(){
+      this.product.timeleft = this.product.vitesse;
+      this.lastupdate = Date.now();
+  }
+
+  calcScore(){
+    if(this.product.timeleft != 0){
+
+      let timepast = Date.now() - this.lastupdate;
+      this.product.timeleft -= timepast;
+
+      if(this.product.timeleft < 0){
+        this.product.timeleft = 0;
+      }
+      else if(this.product.timeleft == 0){
+        this.progressbarvalue = 0
+      }
+      else{
+        this.progressbarvalue = ((this.product.vitesse - this.product.timeleft) / this.product.vitesse) * 100;
+      }
+    }
+  }
+
+  @Output() notifyProduction: EventEmitter<Product> = new
+  EventEmitter<Product>();
+
+  ngOnInit(): void {
+    setInterval(() => { this.calcScore(); }, 100);
   }
 }
